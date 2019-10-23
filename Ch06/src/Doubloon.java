c01c1b8c188d814791401c39dd85d5c5766bdeba
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2019-10-23 15:13:19 -0300

    Add files via upload

--- Ch06/src/Doubloon.java
+++ Ch06/src/Doubloon.java
@@ -1,11 +1,11 @@
 
 public class Doubloon {
-
 	public static void main(String[] args) {
-		System.out.println(isDoubloon("toto"));
+		System.out.println(isDoubloon("abba"));
 	}
 	public static boolean isDoubloon(String s) {
 		for(int i = 0; i < s.length(); i++) {
+			s = s.toLowerCase();
 			int howMany = 0;
 			for(int q = 0; q < s.length(); q++) {
 				if(s.charAt(i) == s.charAt(q)) {
@@ -18,5 +18,4 @@ public class Doubloon {
 		}
 		return true;
 	}
-
 }
