575cb6d898e4ba65b5a500ae9b21e6114e76218c
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2019-11-05 09:18:45 -0400

    Add files via upload

--- Ch07/src/Sieve.java
+++ Ch07/src/Sieve.java
@@ -2,15 +2,21 @@ import java.util.Arrays;
 
 public class Sieve {
 	public static void main(String[] args) {
-		int n = 5;
+		int n = 10;
 		System.out.println(Arrays.toString(sieve(n)));
 	}
 	public static boolean[] sieve(int n) {
-		boolean[] a = new boolean[n];
-		for(int i=0;i<a.length;i++) {
+		boolean[] a = new boolean[n+1];
+		for(int i=2;i<a.length;i++) {
 			a[i] = true;
 		}
-		//for(int i=2;i<)
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(a[i]==true) {
+				for(int j=i*i;j<=n;j+=i) {
+					a[j]=false;
+				}
+			}
+		}
 		return a;
 	}
 }
