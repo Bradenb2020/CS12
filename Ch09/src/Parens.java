03688dd86d270400d29fed36c036ca83bef85309
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2019-11-26 10:48:51 -0400

    Add files via upload

--- Ch09/src/Parens.java
+++ Ch09/src/Parens.java
@@ -2,21 +2,27 @@
  * Exercise on encapsulation and generalization.
  */
 public class Parens {
-
     public static void main(String[] args) {
         String s = "((3 + 7) * 2)";
-        int count = 0;
+        char a = '3';
+        char b = '7';
+        System.out.println(equalChars(s,a,b));
+        System.out.println(balanced(s));
+    }
+    public static int equalChars(String equation, char char1, char char2) {
+    	int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
+        for (int i = 0; i < equation.length(); i++) {
+            char c = equation.charAt(i);
+            if (c == char1) {
                 count++;
-            } else if (c == ')') {
+            } else if (c == char2) {
                 count--;
             }
         }
-
-        System.out.println(count);
+        return count;
     }
-
+    public static int balanced(String equation) {
+    	return equalChars(equation,'(',')');
+    }
 }
--- Ch09/src/Pow.java
+++ Ch09/src/Pow.java
@@ -1,23 +1,27 @@
 /**
  * BigInteger exercise.
  */
+import java.math.BigInteger;
 public class Pow {
+	public static void main(String[] args) {
+		System.out.println(pow(99,99));
+	}
 
     /**
      * Integer exponentiation.
      */
-    public static int pow(int x, int n) {
-        if (n == 0) return 1;
+    public static BigInteger pow(int x, int n) {
+        if (n == 0) return BigInteger.valueOf(1);
 
         // find x to the n/2 recursively
-        int t = pow(x, n / 2);
+        BigInteger t = pow(x, n / 2);
 
         // if n is even, the result is t squared
         // if n is odd, the result is t squared times x
         if (n % 2 == 0) {
-            return t * t;
+            return t.multiply(t);
         } else {
-            return t * t * x;
+            return (t.multiply(t)).multiply(BigInteger.valueOf(x));
         }
     }
 
