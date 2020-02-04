39581e2c5fd0323e9577ec125d3ee12add615a0b
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2020-02-03 23:15:29 -0400

    Update CardTester.java

--- Crazy8s/src/CardTester.java
+++ Crazy8s/src/CardTester.java
@@ -17,4 +17,17 @@ public class CardTester {
 	        System.out.println(card);
 	    }
 	}
+	public static int[] suitHistogram(Card[] cards) {
+	    int[] suitCount=new int{0,0,0,0};
+	    for(int i=0;i<cards.length;i++) {
+		suitCount[cards[i].suit]++;
+	    }
+	    return suitCount;
+	}
+	public static boolean hasFlush(int[] suitCount) {
+	    for(int i=0;i<suitCount.length;i++) {
+		if(suitCount[i]>=5) return true;
+	    }
+	    return false;
+	}
 }
