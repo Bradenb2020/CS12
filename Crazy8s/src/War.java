bfdbe52b89189f4047f7754a01ce65a9704688e3
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2020-02-28 09:09:41 -0400

    Add files via upload

--- Crazy8s/src/War.java
+++ Crazy8s/src/War.java
@@ -11,27 +11,38 @@ public class War {
 			Card c1=p1.popCard();
 			Card c2=p2.popCard();
 			int dif=c1.getRank()-c2.getRank();
-			if(dif>0) {
-				p1.addCard(c1);
-				p1.addCard(c2);
-			} else if(dif<0) {
-				p2.addCard(c1);
-				p2.addCard(c2);
-			} else {
+			Pile war=new Pile();
+			while(dif==0) {
 				if(p1.size()<4) {
 					System.out.println("Player 2 wins");
-					break;
-				} else if (p2.size()<4) {
+				} else if(p2.size()<4) {
 					System.out.println("Player 1 wins");
-					break;
 				} else {
-					
+					war.addCard(c1);
+					war.addCard(c2);
+					for(int i=0;i<3;i++) {
+						war.addCard(p1.popCard());
+						war.addCard(p2.popCard());
+					}
+					c1=p1.popCard();
+					c2=p2.popCard();
+					dif=c1.getRank()-c2.getRank();
 				}
+			}
+			if(dif>0) {
+				while(war.size()>0) p1.addCard(war.popCard());
+				p1.addCard(c1);
+				p1.addCard(c2);
+				
+			} else if(dif<0) {
+				while(war.size()>0) p2.addCard(war.popCard());
+				p2.addCard(c1);
+				p2.addCard(c2);
 			}
 		}
 		if(p1.size()>0) {
 			System.out.println("Player 1 wins");
-		} else {
+		} else if(p2.size()>0){
 			System.out.println("Player 2 wins");
 		}
 	}
